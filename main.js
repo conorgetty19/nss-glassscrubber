6bffb21917c0d41bb696f5bac8eff34786513ad7
Conor Getty <conorgetty@gmail.com>
2023-04-20 10:54:11 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const dishes = [
+    "Dinner plate", "Water glass", "Salad bowl",
+    "Dinner plate", "Wine glass", "Spoon",
+    "Spoon", "Fork", "Salad Bowl", "Whiskey glass",
+    "Fork", "Spoon", "Fork"
+]
+
+let glassOne = dishes[1]
+let glassTwo = dishes[4]
+let glassThree = dishes[9]
+
+console.log("I am cleaning the follow glasses:")
+
+console.log(glassOne)
+console.log(glassTwo)
+console.log(glassThree)
